83cf4d7d48778493125db2da1e918b6666cef708
Dylan Elliott <dylan.elliott@gmail.com>
2024-07-11 19:27:42 +1000

    Initial Commit

---
+++ Director/DirectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectorApp.swift
+//  Director
+//
+//  Created by Dylan Elliott on 11/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct DirectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
